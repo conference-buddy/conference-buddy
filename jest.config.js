63153d791c232b2b24775dc30bb84738402ddb96
Mirjam Aulbach <me@programmiri.rocks>
2021-11-30 20:55:26 +0100

    Remove unneeded configs. Disable afterEach cleanup testing library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+// enables our custom setup and teardown for rendering vue components
+// see: https://gitlab.com/ryter/ryter/-/blob/master/documentation/frontend/testing/03_tools-and-setup/in-depth/custom-cleanup.adoc#user-content-our-setup-and-teardown-explained
+process.env.VTL_SKIP_AUTO_CLEANUP = "true"
+
 module.exports = {
   rootDir: ".",
   globals: {
@@ -17,7 +21,7 @@ module.exports = {
   testPathIgnorePatterns: ["<rootdir>/node_modules/"],
   setupFiles: [`<rootDir>/tests/setup/loadershim.js`],
   setupFilesAfterEnv: ["<rootDir>/tests/setup/setup-after-env.ts"],
-  // testMatch: ["<rootDir>/src/**/*.test.ts"],
+  testMatch: ["<rootDir>/src/**/*.test.ts(x)"],
   moduleDirectories: ["node_modules", "src"],
   testURL: `http://localhost`,
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
