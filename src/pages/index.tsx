a2f94ae161c2097eead306ac2b2a7c5fe23b614c
Mirjam Aulbach <me@programmiri.rocks>
2022-01-05 21:51:12 +0100

    📝 Add first text draft to start page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,32 @@
 import React, { ReactElement } from "react"
 import { PageLayout } from "../ui-elements/page-layout/PageLayout"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import { Conference } from "../domain/conference/conference-interface"
+import { StartPage } from "../ui-elements/start-page/StartPage"
 
 export default function IndexPage(): ReactElement {
+  const data: {
+    allConference: { nodes: Conference[] }
+  } = useStaticQuery(graphql`
+    {
+      allConference(sort: { fields: start_date, order: ASC }, limit: 3) {
+        nodes {
+          city
+          country
+          description
+          end_date
+          start_date
+          url
+          name
+        }
+      }
+    }
+  `)
+
   return (
     <div>
       <PageLayout title="Conference Buddy">
-        <>
-          <header>
-            <h1 className="display-1">Hello world, this is Conference Buddy</h1>
-          </header>
-          <section>
-            <h2>Pages</h2>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/conference-list">Conference List</Link>
-                </li>
-              </ul>
-            </nav>
-          </section>
-          <button className="btn btn-secondary">test button</button>
-          <section>
-            <h2>Colors</h2>
-            <ul>
-              <li className="text-confbuddy-green">Conference Buddy GREEN</li>
-              <div className="bg-confbuddy-green">test</div>
-              <li className="text-confbuddy-pink">Conference Buddy PINK</li>
-              <div className="bg-confbuddy-pink">test</div>
-              <li className="text-primary">Primary</li>
-              <div className="bg-primary">test</div>
-              <li className="text-secondary">Secondary</li>
-              <div className="bg-secondary">test</div>
-              <li className="text-success">text-success</li>
-              <div className="bg-success">test</div>
-              <li className="text-info">text-info</li>
-              <div className="bg-info">test</div>
-              <li className="text-warning">text-warning</li>
-              <div className="bg-warning">test</div>
-              <li className="text-danger">text-danger</li>
-              <div className="bg-danger">test</div>
-              <li className="text-light">text-light</li>
-              <div className="bg-light">test</div>
-              <li className="text-dark">text-dark</li>
-              <div className="bg-dark">test</div>
-            </ul>
-          </section>
-        </>
+        <StartPage conferences={data.allConference.nodes} />
       </PageLayout>
     </div>
   )
---
+++ src/ui-elements/start-page/StartPage.tsx
@@ -0,0 +1,45 @@
+import React, { ReactElement } from "react"
+import { Link } from "gatsby"
+import { Conference } from "../../domain/conference/conference-interface"
+import { ConferenceList } from "../../domain/conference/list/ConferenceList"
+
+function StartPage({
+  conferences,
+}: {
+  conferences: Conference[]
+}): ReactElement {
+  return (
+    <article className="container">
+      <header>
+        <h1 className="display-1">Conference Buddy</h1>
+        <p className="lead">
+          Let’s make tech events more approachable -{" "}
+          <em>one Buddy at a time</em>.
+        </p>
+      </header>
+
+      <p>
+        Conference Buddy provides a platform to find{" "}
+        <strong>companions for tech conferences</strong>. You can look for one
+        or more person with whom you can attend a conference{" "}
+        <strong>together</strong>. Support each other, find a familiar face
+        between all these strangers. You will feel more at ease and comfortable
+        just knowing you’re not alone. At the same time, you have the
+        opportunity to <strong>help others</strong> to feel better!
+      </p>
+      <h2>How does this work?</h2>
+      <p>
+        You can find a detailed explanation how to use Conference Buddy here:
+        How does this work?
+      </p>
+
+      <section>
+        <h2>Upcoming Conferences</h2>
+        <ConferenceList conferences={conferences} />
+        <Link to="/conferene-list">See all conferences</Link>
+      </section>
+    </article>
+  )
+}
+
+export { StartPage }
