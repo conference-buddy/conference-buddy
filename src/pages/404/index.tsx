4a7bd153101634b731d828283152b15254252a90
Mirjam Aulbach <me@programmiri.rocks>
2022-01-15 15:51:35 +0100

    🐕 update husky pre commit and github actions

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import { PageLayout } from "../../ui-elements/page-layout/PageLayout"
+import { TextLink } from "../../ui-elements/text-link/TextLink"
 
 export default function NotFoundPage() {
   return (
     <PageLayout title="Profile">
       <div className="container">
         <h1>Page not found 🥺</h1>
+        Go to the{" "}
+        <TextLink internal={true} to={"/"}>
+          startpage
+        </TextLink>
+        .
       </div>
     </PageLayout>
   )
