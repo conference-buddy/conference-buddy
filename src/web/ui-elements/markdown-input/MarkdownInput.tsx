89b224e1f29d05d2043f16c516131c8e4e47b932
Mirjam Aulbach <me@programmiri.rocks>
2022-02-20 12:57:49 +0100

    🎨 Add markdown editor component

---
+++ src/web/ui-elements/markdown-input/MarkdownInput.tsx
@@ -0,0 +1,43 @@
+import React, { ReactElement } from "react"
+import MDEditor, { ICommand } from "@uiw/react-md-editor"
+import rehypeSanitize from "rehype-sanitize"
+
+type MarkDownInputProps = {
+  text: string
+}
+
+function MarkdownInput(props: MarkDownInputProps): ReactElement {
+  const { text } = props
+  const [value, setValue] = React.useState(text)
+
+  function updateValue(value: string | undefined) {
+    if (value) {
+      setValue(value)
+    }
+  }
+  function removeCommands(cmd: ICommand): false | ICommand {
+    return cmd && cmd.name && /comment|divider|code|codeBlock/.test(cmd.name)
+      ? false
+      : cmd
+  }
+
+  return (
+    <>
+      <div className="container">
+        <MDEditor
+          value={value}
+          onChange={updateValue}
+          previewOptions={{
+            rehypePlugins: [[rehypeSanitize]],
+          }}
+          visiableDragbar={false}
+          preview="edit"
+          commandsFilter={cmd => removeCommands(cmd)}
+        />
+        <MDEditor.Markdown source={value} rehypePlugins={[[rehypeSanitize]]} />
+      </div>
+    </>
+  )
+}
+
+export { MarkdownInput }
