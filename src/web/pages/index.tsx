10237a6ed562a040aee98eb8890c898e7703b4ac
Mirjam Aulbach <me@programmiri.rocks>
2023-05-16 21:47:41 +0200

    linting and precommit setup

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -1,13 +1,10 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
-
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <h1>
-        Henlo!!
-      </h1>
+      <h1>Henlo!!</h1>
     </main>
   )
 }
