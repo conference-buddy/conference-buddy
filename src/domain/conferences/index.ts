1a33f713b56fc777f8932e552114fc1e16955827
paultrue <pav.foo@icloud.com>
2023-05-20 14:41:23 +0200

    Avoid linting of declaration-files and fix some warnings

--- src/domain/conferences/index.ts
+++ src/domain/conferences/index.ts
@@ -1,3 +1,3 @@
-export { Conference } from "./types/conference-interface"
+export type { Conference } from "./types/conference-interface"
 export { getConferences } from "./api/conferences-api"
 export { getConference } from "./api/conferences-api"
