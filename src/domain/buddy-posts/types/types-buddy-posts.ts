b155dc2356d066c1b4c4bb111e3bd0d7f08239e6
paultrue <pav.foo@icloud.com>
2023-05-21 11:53:11 +0200

    BuddyPosts improvements.

--- src/domain/buddy-posts/types/types-buddy-posts.ts
+++ src/domain/buddy-posts/types/types-buddy-posts.ts
@@ -1,3 +1,6 @@
 import { Database } from "../../_database/types"
 
 export type BuddyPostDB = Database["public"]["Tables"]["buddy_posts"]["Row"]
+export type BuddyPostWithProfile = BuddyPostDB & {
+  profile: { username: string; avatar_url: string | null } | null
+}
