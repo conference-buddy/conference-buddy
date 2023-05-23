ae0716a60eca430fb7847f72238c32bc2df7cfa8
paultrue <pav.foo@icloud.com>
2023-05-24 00:23:17 +0200

    Provide empty state for profile when no conference buddy posts were done yet.

--- src/web/ui-elements/buddy-posts/BuddyPostsOfUser.tsx
+++ src/web/ui-elements/buddy-posts/BuddyPostsOfUser.tsx
@@ -1,34 +1,49 @@
 import React, { ReactElement } from "react"
 import { useBuddyPostsOfUser } from "../../../services/hooks/buddy-post/useBuddyPosts"
 import { BuddyPostOfUser } from "./BuddyPostOfUser"
+import { TextLink } from "../text-link/TextLink"
 
 function BuddyPostsOfUser({ profileId }: { profileId: string }): ReactElement {
   const { data: buddyPosts, isLoading } = useBuddyPostsOfUser(profileId)
 
   return (
     <>
-      {!isLoading && buddyPosts ? (
-        buddyPosts.map((post, index: number) => {
-          return (
-            <div key={index}>
-              <BuddyPostOfUser
-                text={post.text}
-                createdAt={post.created_at}
-                conferenceId={post.conference_id}
-                conferenceName={post.conference?.name}
-              />
-              {buddyPosts.length > 1 && index < buddyPosts.length - 1 && (
-                <hr className="text-confbuddy-pink" />
-              )}
-            </div>
-          )
-        })
-      ) : (
+      {isLoading ? (
         <div className="container text-center">
           <div className="spinner-border text-primary" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
         </div>
+      ) : (
+        <>
+          {buddyPosts && buddyPosts?.length > 0 ? (
+            buddyPosts.map((post, index: number) => {
+              return (
+                <div key={index}>
+                  <BuddyPostOfUser
+                    text={post.text}
+                    createdAt={post.created_at}
+                    conferenceId={post.conference_id}
+                    conferenceName={post.conference?.name}
+                  />
+                  {buddyPosts.length > 1 && index < buddyPosts.length - 1 && (
+                    <hr className="text-confbuddy-pink" />
+                  )}
+                </div>
+              )
+            })
+          ) : (
+            <>
+              <p>
+                No BuddyPost created yet. Visit the{" "}
+                <TextLink to={"/conferences"} internal={true}>
+                  conferences page
+                </TextLink>{" "}
+                to find a conference where you want to join as a buddy
+              </p>
+            </>
+          )}
+        </>
       )}
     </>
   )
