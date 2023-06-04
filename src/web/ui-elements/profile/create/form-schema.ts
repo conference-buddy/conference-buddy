0cd9572d70f78e2821dfb107df610c03b4abfd53
Mirjam Aulbach <me@programmiri.rocks>
2023-06-04 18:39:46 +0200

    Update create profile

---
+++ src/web/ui-elements/profile/create/form-schema.ts
@@ -0,0 +1,58 @@
+import * as z from "zod"
+
+const userNameRegex = /^[a-zA-Z0-9_-]+$/
+
+const createProfileSchema = z.object({
+  about_text: z.string().optional(),
+  email: z
+    .string()
+    .nonempty("Please add a email address for notification.")
+    .email("This does not look like a valid email."),
+  id: z.string(),
+  name: z.string().min(1, { message: "Please add a name." }),
+  provider: z.string(),
+  username: z
+    .string()
+    .min(3, { message: "Please add a user name with at least 3 characters." })
+    .regex(userNameRegex, {
+      message: "Username can only contain letters, numbers, underscore, dash.",
+    }),
+  avatar_image: z.any().optional(),
+  social_links: z.object({
+    github: z
+      .string()
+      .url({ message: "This does not look like a valid url." })
+      .optional()
+      .or(z.literal("")),
+    gitlab: z
+      .string()
+      .url({ message: "This does not look like a valid url." })
+      .optional()
+      .or(z.literal("")),
+    mastodon: z
+      .string()
+      .url({ message: "This does not look like a valid url." })
+      .optional()
+      .or(z.literal("")),
+    linkedin: z
+      .string()
+      .url({ message: "This does not look like a valid url." })
+      .optional()
+      .or(z.literal("")),
+    twitter: z
+      .string()
+      .url({ message: "This does not look like a valid url." })
+      .optional()
+      .or(z.literal("")),
+    website: z
+      .string()
+      .url({ message: "This does not look like a valid url." })
+      .optional()
+      .or(z.literal("")),
+  }),
+})
+
+type CreateProfileFormSchema = z.infer<typeof createProfileSchema>
+
+export { createProfileSchema }
+export type { CreateProfileFormSchema }
